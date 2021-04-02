28371f9d148806035f83fd2359a6ee53f0d6fc6d
iBlurBlur <tanakorn.ngam@gmail.com>
2021-04-02 17:52:35 +0700

    login

--- src/repositories/account.repository.js
+++ src/repositories/account.repository.js
@@ -2,3 +2,10 @@ const db = require('../db/models')
 
 exports.add = async (account) => await db.Accounts.create(account)
 
+exports.findByUsername = async (username) => await db.Accounts.findOne({
+    where: {
+        username
+    }
+})
+
+
