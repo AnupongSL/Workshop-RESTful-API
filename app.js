f1a8d7128975620e0d0f06940622e3dfe6ce0296
iBlurBlur <tanakorn.ngam@gmail.com>
2021-04-02 19:53:09 +0700

    swagger

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
+const port = process.env.PORT || 3000
+const env = process.env.NODE_ENV || "development"
+
 const app = express()
 
+if (env !== 'staging' && env !== 'production') {
+    const swaggerUi = require('swagger-ui-express');
+    const swaggerDocument = require('./src/configs/swagger');
+    app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
+
+
 const corsOptions = {
-    origin: ['http://example.com', 
-           'http://localhost:4200',
-            'https://www.w3schools.com'],
+    origin: ['http://example.com',
+        'http://localhost:4200',
+        'https://www.w3schools.com'],
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
@@ -20,9 +30,6 @@ app.use('/image', express.static('./images'))
 app.use(express.json())
 
 app.use(require('./src/routes/routes'))
-
-const port = process.env.PORT || 3000
-const env = process.env.NODE_ENV || "development"
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
